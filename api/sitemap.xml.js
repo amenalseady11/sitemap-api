bbf9be7d459f49b79dae9224aafb3ed2614f3248
bob <info@zainture.com>
2025-12-26 03:59:40 +0300

    Sitemap API Vercel  v3

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -6,27 +6,6 @@ const supabase = createClient(
 );
 
 export default async function handler(req, res) {
-  try {
-    const { data: products } = await supabase.from("products").select("slug");
-    const SITE_URL = process.env.SITE_URL || "https://salla-ye.store";
-
-    let xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`;
-
-    products.forEach((p) => {
-      xml += `
-  <url>
-    <loc>${SITE_URL}/product/${p.slug}</loc>
-    <priority>0.7</priority>
-  </url>`;
-    });
-
-    xml += "\n</urlset>";
-
-    res.setHeader("Content-Type", "application/xml");
-    res.status(200).send(xml);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
+  const { data } = await supabase.from('products').select('*');
+  res.status(200).json({ count: data.length });
 }
